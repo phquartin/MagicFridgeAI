8c173fe7b221ad669b875ffa74b29db423660443
phquartin <phquartinmatos@sempreceub.com>
2025-07-09 02:50:53 -0300

    feat(controller, service): Criação do service e controller para o FoodItem com metodos Get, Post e Delete.

--- src/main/java/dev/phquartin/magicfridgeai/controller/FoodItemController.java
+++ src/main/java/dev/phquartin/magicfridgeai/controller/FoodItemController.java
@@ -1,7 +1,53 @@
 package dev.phquartin.magicfridgeai.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import dev.phquartin.magicfridgeai.model.FoodItem;
+import dev.phquartin.magicfridgeai.service.FoodItemService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/food")
 public class FoodItemController {
+
+    private final FoodItemService foodItemService;
+    public FoodItemController(FoodItemService foodItemService) {
+        this.foodItemService = foodItemService;
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<FoodItem> addFoodItem(FoodItem foodItem) {
+        FoodItem saved = foodItemService.save(foodItem);
+        return ResponseEntity.ok(saved);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<FoodItem>> getAllFoodItems() {
+        List<FoodItem> foodItems = foodItemService.findAll();
+        return ResponseEntity.ok(foodItems);
+    }
+
+    @GetMapping("/id/{id}")
+    public ResponseEntity<FoodItem> getFoodItemById(@PathVariable Long id) {
+        try {
+            FoodItem foodItem = foodItemService.findById(id);
+            return ResponseEntity.ok(foodItem);
+        } catch (Exception e){
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Void> deleteFoodItem(@PathVariable Long id) {
+        try {
+            foodItemService.deleteById(id);
+            return ResponseEntity.ok().build();
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+
+
 }
--- src/main/java/dev/phquartin/magicfridgeai/service/FoodItemService.java
+++ src/main/java/dev/phquartin/magicfridgeai/service/FoodItemService.java
@@ -1,7 +1,34 @@
 package dev.phquartin.magicfridgeai.service;
 
+import dev.phquartin.magicfridgeai.model.FoodItem;
+import dev.phquartin.magicfridgeai.repository.FoodItemRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class FoodItemService {
+
+    private final FoodItemRepository foodItemRepository;
+    public FoodItemService(FoodItemRepository foodItemRepository) {
+        this.foodItemRepository = foodItemRepository;
+    }
+
+    public FoodItem save(FoodItem foodItem) {
+        return foodItemRepository.save(foodItem);
+    }
+
+    public List<FoodItem> findAll() {
+        return foodItemRepository.findAll();
+    }
+    public FoodItem findById(Long id) {
+        return foodItemRepository.findById(id).orElseThrow(EntityNotFoundException::new);
+    }
+
+    public void deleteById(Long id) {
+        findById(id);
+        foodItemRepository.deleteById(id);
+    }
+
 }
