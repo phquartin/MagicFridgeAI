ccf5ea0aedbfc9e52c3b1fe75f8eb51cc2aae308
phquartin <phquartinmatos@sempreceub.com>
2025-07-09 16:17:30 -0300

    chore: Adicionado configs para conexao do banco de dados e conexao com o flyway

---
+++ src/main/resources/db/migration/V1_create_food_item_table.sql
@@ -0,0 +1,18 @@
+CREATE TABLE tb_food_item (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY;
+    Nome VARCHAR(255) NOT NULL;
+    Categoria ENUM(
+        'BEBIDA',
+        'CARNE',
+        'LATICINIO',
+        'FRUTA',
+        'VEGETAL',
+        'GRAO',
+        'DOCE',
+        'CONGELADO',
+        'PADARIA',
+        'MASSA'
+    ) NOT NULL,
+    Quantidade INT NOT NULL,
+    Validade DATE NOT NULL
+);
